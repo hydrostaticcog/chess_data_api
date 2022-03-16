210778a06901707aa9702c6499313b43432b068e
hydrostaticcog <hydro@hydrostaticcog.me>
2022-03-16 17:14:06 -0500

    condense migrations because limigrations is stupid

--- migrations/2022-03-15_16-20-03.py
+++ migrations/2022-03-15_16-20-03.py
@@ -10,7 +10,7 @@ class Migration(BaseMigration):
         """Run when calling 'migrate'."""
         # Do something with connection and cursor
         c.execute(
-            """CREATE TABLE IF NOT EXISTS teams (
+            """CREATE TABLE teams (
             id INTEGER PRIMARY KEY,
             name TEXT,
             sponsor_name TEXT
@@ -18,7 +18,7 @@ class Migration(BaseMigration):
         """
         )
         c.execute(
-            """CREATE TABLE IF NOT EXISTS players (
+            """CREATE TABLE players (
             id INTEGER PRIMARY KEY,
             name TEXT,
             grade TEXT,
@@ -30,7 +30,7 @@ class Migration(BaseMigration):
         """
         )
         c.execute(
-            """CREATE TABLE IF NOT EXISTS officials (
+            """CREATE TABLE officials (
             id INTEGER PRIMARY KEY,
             name TEXT,
             email TEXT,
@@ -39,7 +39,7 @@ class Migration(BaseMigration):
         """
         )
         c.execute(
-            """CREATE TABLE IF NOT EXISTS tournaments (
+            """CREATE TABLE tournaments (
                 id INTEGER PRIMARY KEY,
                 name TEXT,
                 date INTEGER,
@@ -50,7 +50,7 @@ class Migration(BaseMigration):
             """
         )
         c.execute(
-            """CREATE TABLE IF NOT EXISTS games (
+            """CREATE TABLE games (
                 id INTEGER PRIMARY KEY,
                 tournament_id INTEGER,
                 board INTEGER,
@@ -64,6 +64,28 @@ class Migration(BaseMigration):
                 FOREIGN KEY (official) REFERENCES officials(id)
             )
             """
+        )
+        c.execute(
+            """CREATE TABLE IF NOT EXISTS enrollment (
+            id INTEGER PRIMARY KEY,
+            player_id INTEGER,
+            tournament_id INTEGER,
+            team_id INTEGER,
+            FOREIGN KEY (player_id) REFERENCES players(id),
+            FOREIGN KEY (tournament_id) REFERENCES tournaments(id),
+            FOREIGN KEY (team_id) REFERENCES teams(id)
+            )
+        """)
+        c.execute(
+            """CREATE UNIQUE INDEX unique_index ON enrollment(player_id, tournament_id)""")
+        c.execute(
+            """ALTER TABLE games ADD COLUMN round INTEGER"""
+        )
+        c.execute(
+            """ALTER TABLE tournaments ADD COLUMN rounds INTEGER NOT NULL DEFAULT 3"""
+        )
+        c.execute(
+            """ALTER TABLE tournaments ADD COLUMN boards INTEGER NOT NULL DEFAULT 10"""
         )
         conn.commit()
         pass
