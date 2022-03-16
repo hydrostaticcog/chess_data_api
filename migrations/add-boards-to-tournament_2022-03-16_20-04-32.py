8646a8aabb801283cc3300d33683badd64802b96
hydrostaticcog <hydro@hydrostaticcog.me>
2022-03-16 15:08:35 -0500

    add boards to tournament migration

--- migrations/add-boards-to-tournament_2022-03-16_20-04-32.py
+++ migrations/add-boards-to-tournament_2022-03-16_20-04-32.py
@@ -10,16 +10,8 @@ class Migration(BaseMigration):
         """Run when calling 'migrate'."""
         # Do something with connection and cursor
         c.execute(
-            """CREATE TABLE IF NOT EXISTS enrollment (
-            id INTEGER PRIMARY KEY,
-            player_id INTEGER,
-            tournament_id INTEGER,
-            team_id INTEGER,
-            FOREIGN KEY (player_id) REFERENCES players(id),
-            FOREIGN KEY (tournament_id) REFERENCES tournaments(id),
-            FOREIGN KEY (team_id) REFERENCES teams(id)
-            )
-        """)
+            """ALTER TABLE tournaments ADD COLUMN boards INTEGER"""
+        )
         pass
 
     def down(self, conn, c):
